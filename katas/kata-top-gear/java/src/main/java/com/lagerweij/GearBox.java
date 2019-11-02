7ee37f2cb482d59667c18a7a7932c5d483a0f0e9
Bianca Carnevale <bianca.carnevale@myob.com>
2019-11-02 16:23:28 +1100

    Update GearBox.java (#123)
    Remove bug in java code that makes the exercise annoying as a teaching tool [Sol]

--- katas/kata-top-gear/java/src/main/java/com/lagerweij/GearBox.java
+++ katas/kata-top-gear/java/src/main/java/com/lagerweij/GearBox.java
@@ -34,16 +34,20 @@ public class GearBox {
 			e = i;
 		} else {
 			if (s > 0) {
-				if (i > 2000)
+				if (i > 2000) {
 					s++;
 				} else if (i < 500) {
 					s--;
 				}
-			} if (s > 6) {
-				s--;
-			} else if (s < 1) {
-				s++;
 			}
-			e = i;
+		}	
+		
+		if (s > 6) {
+			s--;
+		} else if (s < 1) {
+			s++;
 		}
+		
+		e = i;
+	}
 }
