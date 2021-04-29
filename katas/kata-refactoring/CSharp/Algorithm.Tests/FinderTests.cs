28c40c95d2c17ead2ba09ada55692b43c1e20bff
Leah Filkin <leah.filkin@myob.com>
2021-04-29 15:22:25 +1000

    renamed everything

--- katas/kata-refactoring/CSharp/Algorithm.Tests/FinderTests.cs
+++ katas/kata-refactoring/CSharp/Algorithm.Tests/FinderTests.cs
@@ -9,78 +9,78 @@ namespace Algorithm.Test
         [Fact]
         public void Returns_Empty_Results_When_Given_Empty_List()
         {
-            var list = new List<Thing>();
-            var finder = new Finder(list);
+            var people = new List<Person>();
+            var ageGapCalculator = new AgeGapCalculator(people);
 
-            var result = finder.Find(FT.One);
+            var result = ageGapCalculator.FindAgeGap(ChoiceOfAgeGapLength.SmallestAgeGap);
 
-            Assert.Null(result.P1);
-            Assert.Null(result.P2);
+            Assert.Null(result.YoungerPerson);
+            Assert.Null(result.OlderPerson);
         }
 
         [Fact]
         public void Returns_Empty_Results_When_Given_One_Person()
         {
-            var list = new List<Thing>() { sue };
-            var finder = new Finder(list);
+            var people = new List<Person>() { sue };
+            var ageGapCalculator = new AgeGapCalculator(people);
 
-            var result = finder.Find(FT.One);
+            var result = ageGapCalculator.FindAgeGap(ChoiceOfAgeGapLength.SmallestAgeGap);
 
-            Assert.Null(result.P1);
-            Assert.Null(result.P2);
+            Assert.Null(result.YoungerPerson);
+            Assert.Null(result.OlderPerson);
         }
 
         [Fact]
         public void Returns_Closest_Two_For_Two_People()
         {
-            var list = new List<Thing>() { sue, greg };
-            var finder = new Finder(list);
+            var people = new List<Person>() { sue, greg };
+            var ageGapCalculator = new AgeGapCalculator(people);
 
-            var result = finder.Find(FT.One);
+            var result = ageGapCalculator.FindAgeGap(ChoiceOfAgeGapLength.SmallestAgeGap);
 
-            Assert.Same(sue, result.P1);
-            Assert.Same(greg, result.P2);
+            Assert.Same(sue, result.YoungerPerson);
+            Assert.Same(greg, result.OlderPerson);
         }
 
         [Fact]
         public void Returns_Furthest_Two_For_Two_People()
         {
-            var list = new List<Thing>() { greg, mike };
-            var finder = new Finder(list);
+            var people = new List<Person>() { greg, mike };
+            var ageGapCalculator = new AgeGapCalculator(people);
 
-            var result = finder.Find(FT.Two);
+            var result = ageGapCalculator.FindAgeGap(ChoiceOfAgeGapLength.LargestAgeGap);
 
-            Assert.Same(greg, result.P1);
-            Assert.Same(mike, result.P2);
+            Assert.Same(greg, result.YoungerPerson);
+            Assert.Same(mike, result.OlderPerson);
         }
 
         [Fact]
         public void Returns_Furthest_Two_For_Four_People()
         {
-            var list = new List<Thing>() { greg, mike, sarah, sue };
-            var finder = new Finder(list);
+            var people = new List<Person>() { greg, mike, sarah, sue };
+            var ageGapCalculator = new AgeGapCalculator(people);
 
-            var result = finder.Find(FT.Two);
+            var result = ageGapCalculator.FindAgeGap(ChoiceOfAgeGapLength.LargestAgeGap);
 
-            Assert.Same(sue, result.P1);
-            Assert.Same(sarah, result.P2);
+            Assert.Same(sue, result.YoungerPerson);
+            Assert.Same(sarah, result.OlderPerson);
         }
 
         [Fact]
         public void Returns_Closest_Two_For_Four_People()
         {
-            var list = new List<Thing>() { greg, mike, sarah, sue };
-            var finder = new Finder(list);
+            var people = new List<Person>() { greg, mike, sarah, sue };
+            var ageGapCalculator = new AgeGapCalculator(people);
 
-            var result = finder.Find(FT.One);
+            var result = ageGapCalculator.FindAgeGap(ChoiceOfAgeGapLength.SmallestAgeGap);
 
-            Assert.Same(sue, result.P1);
-            Assert.Same(greg, result.P2);
+            Assert.Same(sue, result.YoungerPerson);
+            Assert.Same(greg, result.OlderPerson);
         }
 
-        Thing sue = new Thing() {Name = "Sue", BirthDate = new DateTime(1950, 1, 1)};
-        Thing greg = new Thing() {Name = "Greg", BirthDate = new DateTime(1952, 6, 1)};
-        Thing sarah = new Thing() { Name = "Sarah", BirthDate = new DateTime(1982, 1, 1) };
-        Thing mike = new Thing() { Name = "Mike", BirthDate = new DateTime(1979, 1, 1) };
+        Person sue = new Person() {Name = "Sue", BirthDate = new DateTime(1950, 1, 1)};
+        Person greg = new Person() {Name = "Greg", BirthDate = new DateTime(1952, 6, 1)};
+        Person sarah = new Person() { Name = "Sarah", BirthDate = new DateTime(1982, 1, 1) };
+        Person mike = new Person() { Name = "Mike", BirthDate = new DateTime(1979, 1, 1) };
     }
 }
---
+++ katas/kata-refactoring/CSharp/Algorithm/Algorithm/AgeGapCalculator.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithm
+{
+    public class AgeGapCalculator
+    {
+        private readonly List<Person> _people;
+
+        public AgeGapCalculator(List<Person> people)
+        {
+            _people = people;
+        }
+
+        public PeopleAndTheirAgeGaps FindAgeGap(ChoiceOfAgeGapLength choiceOfAgeGapLength)
+        {
+            var listOfAgeGaps = new List<PeopleAndTheirAgeGaps>();
+
+            for(var i = 0; i < _people.Count - 1; i++)
+            {
+                for(var j = i + 1; j < _people.Count; j++)
+                {
+                    var r = new PeopleAndTheirAgeGaps();
+                    if(_people[i].BirthDate < _people[j].BirthDate)
+                    {
+                        r.YoungerPerson = _people[i];
+                        r.OlderPerson = _people[j];
+                    }
+                    else
+                    {
+                        r.YoungerPerson = _people[j];
+                        r.OlderPerson = _people[i];
+                    }
+                    r.DifferenceInAge = r.OlderPerson.BirthDate - r.YoungerPerson.BirthDate;
+                    listOfAgeGaps.Add(r);
+                }
+            }
+
+            if(listOfAgeGaps.Count < 1)
+            {
+                return new PeopleAndTheirAgeGaps();
+            }
+
+            PeopleAndTheirAgeGaps mostAppropriateAgeGap = listOfAgeGaps[0];
+            foreach(var ageGap in listOfAgeGaps)
+            {
+                switch(choiceOfAgeGapLength)
+                {
+                    case ChoiceOfAgeGapLength.SmallestAgeGap:
+                        if(ageGap.DifferenceInAge < mostAppropriateAgeGap.DifferenceInAge)
+                        {
+                            mostAppropriateAgeGap = ageGap;
+                        }
+                        break;
+
+                    case ChoiceOfAgeGapLength.LargestAgeGap:
+                        if(ageGap.DifferenceInAge > mostAppropriateAgeGap.DifferenceInAge)
+                        {
+                            mostAppropriateAgeGap = ageGap;
+                        }
+                        break;
+                }
+            }
+
+            return mostAppropriateAgeGap;
+        }
+    }
+}
